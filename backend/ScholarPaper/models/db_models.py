eb68659765632a63f203f5e755541c29b37691db
MisterHui <1072127943@qq.com>
2019-06-05 13:50:20 +0800

    Add Document classes Definition

--- backend/ScholarPaper/models/db_models.py
+++ backend/ScholarPaper/models/db_models.py
@@ -14,12 +14,43 @@ connect(
 )
 
 
+"""
+    下面定义了一些MongoDB数据库中的文档结构
+"""
 class User(Document):
     _id = StringField()
     username = StringField()
     password_hash = StringField()
     doc_amount = IntField()
     user_type = StringField()
+
+class Metadata(Document):
+    title = StringField()
+    paper_id = StringField()
+    author = ListField()
+    publish_date = StringField()
+    publish_source = StringField()
+    link_url = URLField()
+    user_score = IntField()
+
+class Documents(Document):
+    _id = StringField()
+    owner_id = StringField()
+    metadata = EmbeddedDocumentField(Metadata)
+    color = IntField()
+    topic = ListField()
+
+class Library(Document):
+    _id = StringField()
+    owner_id = StringField()
+    lib_name = StringField()
+    doc_list = ListField()
+
+class Topic(Document):
+    _id = StringField()
+    topic_name = StringField()
+    doc_list = ListField()
+
 
 
 class API(Resource):
