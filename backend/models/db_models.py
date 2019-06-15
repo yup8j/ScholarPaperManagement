39bd0dba9fc0ee2bf26409e4bc32caefc06481f5
yup8j <shijiahan@tuta.io>
2019-06-15 21:38:04 +0800

    add: __init__.py

--- backend/models/db_models.py
+++ backend/models/db_models.py
@@ -2,15 +2,6 @@ from flask import request, make_response
 from mongoengine import *
 from flask_restful import Resource
 
-connect(
-    db='test_11',
-    host='mongodb://dds-wz9f23f0cffe4b341504-pub.mongodb.rds.aliyuncs.com:3717,dds-wz9f23f0cffe4b342338-pub.mongodb.rds.aliyuncs.com:3717',
-    username='root',
-    password='qwerty2019()-=',
-    authentication_source='admin',
-    authentication_mechanism='SCRAM-SHA-1',
-    replicaset='mgset-15064123'
-)
 
 
 class User(Document):
