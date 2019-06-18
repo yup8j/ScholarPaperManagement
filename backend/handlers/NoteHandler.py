ffd86f65d2e113408cdc0ed27837d317b7d0984f
yup8j <shijiahan@tuta.io>
2019-06-18 21:08:57 +0800

    保存笔记，测试

--- backend/handlers/NoteHandler.py
+++ backend/handlers/NoteHandler.py
@@ -9,7 +9,9 @@ def get_note(document_id, user_id):
     doc_name = doc.save_name
     note_flag = doc.save_note
     if not note_flag:
-        os.mkdir('./' + 'md/' + user_name)
+        path = './' + 'md/' + user_name
+        if not os.path.isdir(path):
+            os.mkdir('./' + 'md/' + user_name)
         # 新建笔记
         with open('./md/' + user_name + '/' + doc_name + '.md', 'a+') as f:
             f.close()
@@ -17,8 +19,21 @@ def get_note(document_id, user_id):
         note = ''
         code = 200
     else:
-        with open('./md/' + user_name + '/' + doc_name + '.md', 'rb') as f:
+        with open('./md/' + user_name + '/' + doc_name + '.md', 'r') as f:
             note = f.read()
             f.close()
             code = 200
     return note, code
+
+
+def save_note(document_id, user_id, note_content):
+    user = User.objects(id=str(user_id)).first()
+    user_name = user.username
+    doc = Documents.objects(id=str(document_id)).first()
+    doc_name = doc.save_name
+    path = './' + 'md/' + user_name
+    with open('./md/' + user_name + '/' + doc_name + '.md', 'a+') as f:
+        f.write(note_content)
+        f.close()
+    code = 200
+    return code
--- backend/resources/notes.py
+++ backend/resources/notes.py
@@ -1,5 +1,5 @@
 from backend.resources import *
-from backend.handlers.NoteHandler import get_note
+from backend.handlers.NoteHandler import *
 
 
 class GetNote(API):
@@ -14,7 +14,27 @@ class GetNote(API):
         document_id = args['document_id']
         user_id = '5cf0c31890f43a4e53492b34'
         note, code = get_note(document_id=document_id, user_id=user_id)
+        note = note.replace('\n', '<br/>')
         self.response = make_response()
         self.response.status_code = code
         self.response = jsonify({"note": str(note)})
+        return self.response
+
+
+class SaveNote(API):
+    """
+    保存笔记
+    """
+
+    def post(self):
+        parse = reqparse.RequestParser()
+        parse.add_argument('document_id', type=str)
+        parse.add_argument('note_content', type=str)
+        args = parse.parse_args()
+        document_id = args['document_id']
+        user_id = '5cf0c31890f43a4e53492b34'
+        note_content = args['note_content']
+        code = save_note(document_id=document_id, user_id=user_id, note_content=note_content)
+        self.response = make_response()
+        self.response.status_code = code
         return self.response
