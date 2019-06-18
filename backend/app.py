d44579b33d8922c1f0eb41480830be90adff5611
yup8j <shijiahan@tuta.io>
2019-06-18 17:50:44 +0800

    import放到init

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from flask_restful import Api
 from backend.resources.login import *
 from backend.resources.oss_control import *
+from backend.resources.edit_info import *
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
--- backend/resources/oss_control.py
+++ backend/resources/oss_control.py
@@ -1,10 +1,4 @@
-from backend.models.db_models import API
-from flask_restful import reqparse
-import werkzeug.datastructures
-from flask import Flask, json, request, make_response, jsonify, send_file
-from backend.utils.oss import auth, bucket
-from backend.handlers.DocHandler import upload, download, delete_document
-import io
+from backend.resources import *
 
 
 class UploadDocuments(API):
