66298fb19a4973b4eb654ab84a97e26b80458a4b
yup8j <shijiahan@tuta.io>
2019-06-19 23:12:23 +0800

    request_getjson

--- backend/resources/oss_control.py
+++ backend/resources/oss_control.py
@@ -42,10 +42,12 @@ class DeleteDocuments(API):
     """
     # TODO：完善lib相关部分的删除
     def post(self):
+        request.get_json(force=True)
         parse = reqparse.RequestParser()
         parse.add_argument('document_id', type=str)
         args = parse.parse_args()
         document_id = args['document_id']
+        print(document_id)
         self.response = make_response()
         try:
             delete_document(document_id=document_id, user_id='5cf0c31890f43a4e53492b34')
