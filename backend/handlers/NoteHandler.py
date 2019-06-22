d65d58e1f75f9e656593d556d8d13236eae3f13d
yup8j <shijiahan@tuta.io>
2019-06-22 18:42:06 +0800

    note

--- backend/handlers/NoteHandler.py
+++ backend/handlers/NoteHandler.py
@@ -9,17 +9,17 @@ def get_note(document_id, user_id):
     doc_name = doc.save_name
     note_flag = doc.save_note
     if not note_flag:
-        path = '../' + 'md/' + user_name
+        path = './md/' + user_name + '/'
         if not os.path.isdir(path):
             os.makedirs(path)
         # 新建笔记
-        with open('../md/' + user_name + '/' + doc_name + '.md', 'a+') as f:
+        with open('./md/' + user_name + '/' + doc_name + '.md', 'a+') as f:
             f.close()
         doc.update(set__save_note=1)
         note = ''
         code = 200
     else:
-        with open('../md/' + user_name + '/' + doc_name + '.md', 'r') as f:
+        with open('./md/' + user_name + '/' + doc_name + '.md', 'r+') as f:
             note = f.read()
             f.close()
             code = 200
@@ -31,8 +31,8 @@ def save_note(document_id, user_id, note_content):
     user_name = user.username
     doc = Documents.objects(id=str(document_id)).first()
     doc_name = doc.save_name
-    path = '../' + 'md/' + user_name
-    with open('../md/' + user_name + '/' + doc_name + '.md', 'a+') as f:
+    path = './' + 'md/' + user_name
+    with open('./md/' + user_name + '/' + doc_name + '.md', 'a+') as f:
         f.write(note_content)
         f.close()
     code = 200
