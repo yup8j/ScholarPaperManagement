ebeadb967ed2d7423f46d14272acfd15f8095a84
yup8j <shijiahan@tuta.io>
2019-06-26 11:13:19 +0800

    new file:   backend/test/NoteHandlerTest.py

--- backend/test/DocHandlerTest.py
+++ backend/test/DocHandlerTest.py
@@ -1,1 +1,7 @@
-import unittest
+from backend.test import *
+
+
+class TestDocHandler(unittest.TestCase):
+    def test_delete(self):
+        self.assertEqual(delete_document(document_id='5d0f2f7c3e02f9e99c246720', user_id='5d0f29a33e02f9e99c246717'),
+                         200)
